6d7a2d72ba5da4eb5e0a342cbb94fc575375cc2e
Dima Stadub <Dev@eml.by>
2022-10-20 01:52:12 +0300

    iikoFront Order models

---
+++ alivery/Alivery.Db/Model/Order.cs
@@ -0,0 +1,32 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Alivery.Db.Model.Order
+// Assembly: Alivery.Db, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 9F42CC1A-62E5-44F7-BF78-501FFB7A8F4A
+// Assembly location: C:\Cache\45d56e93\Downloads\alivery.net\Debug\Alivery.Db.dll
+
+using SqlBase;
+using SQLite;
+using System;
+
+namespace Alivery.Db.Model
+{
+  public class Order : IValueObject
+  {
+    [PrimaryKey]
+    public string Id { get; set; }
+
+    public string Json { get; set; }
+
+    public OrderStatus OrderStatus { get; set; }
+
+    [Indexed]
+    public int Revision { get; set; }
+
+    public DateTime OpenTime { get; set; }
+
+    public DateTime? CloseTime { get; set; }
+
+    [Indexed]
+    public string IikoOrderId { get; set; }
+  }
+}
---
+++ alivery/Alivery.Db/Model/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Alivery.Db.Model.OrderStatus
+// Assembly: Alivery.Db, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 9F42CC1A-62E5-44F7-BF78-501FFB7A8F4A
+// Assembly location: C:\Cache\45d56e93\Downloads\alivery.net\Debug\Alivery.Db.dll
+
+namespace Alivery.Db.Model
+{
+  public enum OrderStatus
+  {
+    New,
+    Bill,
+    Closed,
+    Deleted,
+  }
+}
